05965b681ec419303a667202bcd17126c60f98f2
JeonHywn <wjsgusdn1tp@naver.com>
2025-11-21 12:54:10 +0900

    feat : 헤더 스크롤 시 색 변경

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "./common.css";
+import { useEffect, useRef } from "react";
 const Header = () => {
   // 네비게이션 항목
   const navItems = [
@@ -9,8 +10,41 @@ const Header = () => {
     { name: "My Work", to: "#work" },
   ];
 
+  // header DOM을 직접 가리키기 위한 "ref" 만들기
+  const headerRef = useRef(null);
+
+  // 스크롤 할때 header에 header--dark 클래스를 붙이기 때기 기능
+  useEffect(() => {
+    // headerRef.current = 실제 헤더 DOM 요소
+    const header = headerRef.current;
+
+    // 만약 ref가 아직 연결 안 됐으면 종료
+    if (!header) return;
+
+    // header의 실제 높이를 가져오기 → 스크롤 기준점
+    const headerHeight = header.getBoundingClientRect().height;
+    console.log(headerHeight);
+
+    const handleScroll = () => {
+      //window.scrollY = 지금 얼마나 아래로 내려왔는지(px)
+      if (window.scrollY > headerHeight) {
+        // 기준보다 더 내려왔으면 → 어두운 모드 ON
+        header.classList.add("header--dark");
+      } else {
+        // 다시 위로 올라가면 → 어두운 모드 OFF
+        header.classList.remove("header--dark");
+      }
+    };
+    // 스크롤 이벤트 등록
+    window.addEventListener("scroll", handleScroll);
+    // 컴포넌트가 사라질 때 이벤트 제거 (메모리 누수 방지)
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
       <div className="header_logo">
         <img
           className="header_logo_img"
