148c9e5194e7a614c231b0737abfebe74edd90d5
JeonHywn <wjsgusdn1tp@naver.com>
2025-11-24 16:12:25 +0900

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter basename="/hyunwoo-portfolio">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
