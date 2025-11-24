877a80bd416c7bf56fa0ceec079ad0122d9945a8
JeonHywn <wjsgusdn1tp@naver.com>
2025-11-24 16:10:19 +0900

    fix: change to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/hyunwoo-portfolio">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
